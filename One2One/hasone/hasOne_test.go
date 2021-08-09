5a35549af20355bc6196a75b3f6d7a841424ad4f
fan <1066580119@qq.com>
2021-08-09 21:54:05 +0800

    hasOne相关用法

--- One2One/hasone/hasOne_test.go
+++ One2One/hasone/hasOne_test.go
@@ -1,20 +1,66 @@
 package hasone
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"testing"
+)
 
-// 定义一个玩家结构
-type Player struct {
-	gorm.Model
-	UID 		uint 		`gorm:"unique;comment:玩家唯一id"`
-	Username 	string 		`gorm:"comment:玩家用户名"`
-	Password	string		`gorm:"comment:密码"`
+var(
+	dsn = "root:1234@(127.0.0.1:3306)/SimpleGorm?charset=utf8mb4&parseTime=True&loc=Local"
+	db, _ = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		DisableForeignKeyConstraintWhenMigrating: true,	//禁用物理外键
+	})
+)
+
+
+// hasOne数据表迁移测试，默认外键,指定外键,自定义引用等等
+func TestAutoMigrate(t *testing.T){
+	err := db.AutoMigrate(&Player{},&Role{},&Phone{},&Face{})
+	if err!=nil{
+		fmt.Println("数据表迁移失败")
+	}else{
+		fmt.Println("数据表迁移成功")
+	}
 }
 
 
-// 定义一个游戏角色
-type Role struct {
-	gorm.Model
-	Name 	string 		`gorm:"comment:游戏角色名"`
-	Level 	string		`gorm:"comment:游戏角色登记"`
-	Coins  	uint32		`gorm:"comment:游戏角色金币数"`
+// 测试插入数据
+func TestInsert(t *testing.T){
+	player := Player{
+		UID: 12345,
+		Username: "xiamu",
+		Password: "1234",
+		Role: Role{
+			Name: "夏目",
+			Level: "12",
+			Coins: 100,
+		},
+		Phone: 	Phone{
+			Brand: "xiaomi",
+			Number: 138888888,
+		},
+		Face:	Face{
+			FaceData: "testtest",
+		},
+	}
+
+	result := db.Save(&player)
+	if result.RowsAffected==0 {
+		fmt.Println("插入失败")
+	}else{
+		fmt.Println("插入成功",result.RowsAffected)
+	}
 }
+
+// 删除关联；清空关联
+
+
+// 测试更新数据
+
+
+// 测试添加关联
+
+
+// 测试关联查询数据
--- One2One/hasone/models.go
+++ One2One/hasone/models.go
@@ -1,3 +1,39 @@
 package hasone
 
+import (
+	"gorm.io/gorm"
+)
 
+//------------------------------1-----------------------------------------
+// 定义一个玩家结构
+type Player struct {
+	gorm.Model				// gorm提供的结构体,自带id\Createat\updateat\deleteat
+	UID 		uint 		`gorm:"unique;comment:玩家唯一id"`
+	Username 	string 		`gorm:"comment:玩家用户名"`
+	Password	string		`gorm:"comment:密码"`
+	Role 		Role
+	Phone		Phone		`gorm:"foreignKey:UID"`					// 自定义外键
+	Face		Face		`gorm:"foreignKey:UID;references:uid"`	// 自定义引用
+
+}
+// 定义一个游戏角色 使用playerId作为外键
+type Role struct {
+	gorm.Model
+	PlayerId 	uint		// 默认使用user.uid作为外键
+	Name 		string 		`gorm:"comment:游戏角色名"`
+	Level 		string		`gorm:"comment:游戏角色登记"`
+	Coins  		uint32		`gorm:"comment:游戏角色金币数"`
+}
+// 定义手机结构体 使用uid作为外键
+type Phone struct {
+	gorm.Model
+	Brand     	string		`gorm:"comment:手机型号"`
+	Number    	int			`gorm:"comment:手机号码"`
+	UID			int
+}
+// 定义捏脸数据  使用Username作为外键引用
+type Face struct {
+	gorm.Model
+	FaceData 	string 		 `gorm:"comment:捏脸数据"`
+	UID			uint
+}
